868729cb5335de266c07488e157237628d1d2adc
dabananda <dabananda.dev@gmail.com>
2025-09-22 23:15:30 +0600

    refactor: added extension methods for services

---
+++ MatchUp/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,26 @@
+﻿using MatchUp.Data;
+using MatchUp.Interfaces;
+using MatchUp.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace MatchUp.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(
+            this IServiceCollection services,
+            IConfiguration config)
+        {
+            services.AddControllers();
+            services.AddOpenApi();
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+            });
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddCors();
+
+            return services;
+        }
+    }
+}
--- MatchUp/Program.cs
+++ MatchUp/Program.cs
@@ -1,40 +1,10 @@
-using MatchUp.Data;
-using MatchUp.Interfaces;
-using MatchUp.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
+using MatchUp.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-builder.Services.AddDbContext<DataContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-builder.Services.AddScoped<ITokenService, TokenService>();
-
-builder.Services.AddCors();
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        var tokenKey = builder.Configuration["TokenKey"] ?? throw new Exception("TokenKey not found");
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
-            ValidateIssuer = false,
-            ValidateAudience = false
-        };
-    });
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
