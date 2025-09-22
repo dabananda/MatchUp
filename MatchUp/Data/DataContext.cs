e048dec22cbe312c4c7683945f631aee0b174cd9
dabananda <dabananda.dev@gmail.com>
2025-09-22 14:13:44 +0600

    feat: primary setup with user model and connection string

---
+++ MatchUp/Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using MatchUp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MatchUp.Data
+{
+    public class DataContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
