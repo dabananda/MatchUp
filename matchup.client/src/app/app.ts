5a29b08ec894a5269e37dff721077eee1aa6f5c8
dabananda <dabananda.dev@gmail.com>
2025-09-22 15:14:01 +0600

    feat: added fetch request to users

--- matchup.client/src/app/app.ts
+++ matchup.client/src/app/app.ts
@@ -1,11 +1,22 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   imports: [],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
+export class App implements OnInit {
+  http = inject(HttpClient);
   protected readonly title = signal('MatchUp: The BEST Soulmate Finder');
+  users: any;
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:5205/api/users').subscribe({
+      next: (response) => (this.users = response),
+      error: (error) => console.log(error),
+      complete: () => console.log('Request completed!'),
+    });
+  }
 }
