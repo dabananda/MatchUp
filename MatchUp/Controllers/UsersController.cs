dc2a06069a8ee5be9fd6d752d1e9778fdcbeccbe
dabananda <dabananda.dev@gmail.com>
2025-09-22 18:59:50 +0600

    feat: added base controller and refactor the existing controller

--- MatchUp/Controllers/UsersController.cs
+++ MatchUp/Controllers/UsersController.cs
@@ -5,9 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace MatchUp.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UsersController(DataContext context) : ControllerBase
+    public class UsersController(DataContext context) : BaseController
     {
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
