5a89a3553bf0475c494b9a3cbd43429eec7caa23
dabananda <dabananda.dev@gmail.com>
2025-09-24 09:30:54 +0600

    feat: middleware and error handling

---
+++ MatchUp/Controllers/BuggyController.cs
@@ -0,0 +1,22 @@
+﻿using MatchUp.Data;
+using MatchUp.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MatchUp.Controllers
+{
+    public class BuggyController(DataContext context) : BaseController
+    {
+        [HttpGet("server-error")]
+        public ActionResult<AppUser> GetServerError()
+        {
+            var thing = context.Users.Find(-1) ?? throw new Exception("A bad thing has happened");
+            return thing;
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult<AppUser> GetBadRequest()
+        {
+            return BadRequest("This was not a good request");
+        }
+    }
+}
--- MatchUp/Program.cs
+++ MatchUp/Program.cs
@@ -1,4 +1,5 @@
 using MatchUp.Extensions;
+using MatchUp.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,8 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseHttpsRedirection();
 
