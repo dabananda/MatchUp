23137179b2cc3648c0c118c86317b8b966f99896
dabananda <dabananda.dev@gmail.com>
2025-09-25 08:59:59 +0600

    feat: seed data

--- MatchUp/Program.cs
+++ MatchUp/Program.cs
@@ -1,5 +1,7 @@
+using MatchUp.Data;
 using MatchUp.Extensions;
 using MatchUp.Middleware;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,5 +27,19 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration");
+}
 
 app.Run();
